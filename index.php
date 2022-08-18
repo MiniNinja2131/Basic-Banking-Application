67758d2a1658882d9146f7afd651493408d05c8d
unknown <dannyche@live.co.uk>
2022-08-18 15:10:37 +0100

    basic foundation of our html page

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang = "en" dir = "ltr">
+    <head>
+        <meta charset = "utf-8">
+        <title> Basic Banking Application </title>
+        <link rel = "stylesheet" href = "css/style.css">
+        <script src = "js/script.js"></script>
+    </head>
+
+    <body>
+
+        <nav>
+            <div class = "wrapper">
+                <ul>
+                    <li><a href = "index.php"> Home </a></li>
+                    <li><a href = "about.php"> About Us </a></li>
+                    <li><a href = "signUp.php"> Sign up </a></li>
+                    <li><a href = "login.php"> Login </a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class = "wrapper">
+            <section class = "index-intro">
+                <h1> Basic Banking Application </h1>
+                <p> Welcome to our basic banking application where you can: </br>1) Check your balance </br>2) Make a deposit </br>3) Make a withdrawal </br>4) View previous transaction </br>5) Calculate interest </p>
+            </section>
+
+            <section class = "index-categories">
+                <h2> Banking Options </h2>
+
+                <div class = "index-categproes-list">
+                    <div>
+                        <h3> Check Balance </h3>
+                    </div>
+
+                    <div>
+                        <h3> Make a deposit </h3>
+                    </div>
+
+                    <div>
+                        <h3> Make a deposit to another account </h3>
+                    </div>
+
+                    <div>
+                        <h3> Make a withdrawal </h3>
+                    </div>
+
+                    <div>
+                        <h3> View previous transaction </h3>
+                    </div>
+
+                    <div>
+                        <h3> Calculate interest </h3>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+    </html>
+  
