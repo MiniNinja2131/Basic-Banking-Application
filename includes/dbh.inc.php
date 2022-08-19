47a6a30b91d88775e4c4c65f22091a4a70c46a67
unknown <dannyche@live.co.uk>
2022-08-19 16:45:42 +0100

    connected to the database via mysqli and updated database erd

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,1 +1,16 @@
 <!-- Database Handler -->
+<?php
+
+$serverName = "localhost";
+$dbName = "basicBankDB";
+$dbUsername = "root";
+/* Xampp pass is defaulted to empty */
+$dbPassword = "";
+
+/* Connect to the database */
+$conn = mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
+
+/* Error message if there was a fail connection to the database */
+if(!$conn){
+    die("Connection failed: " . mysqli_connect_error());
+}
